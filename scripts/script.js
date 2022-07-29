519e91bec7a0e2cf09bce76e690cd02e5451e5de
JustusJBYL <106807309+JustusJBYL@users.noreply.github.com>
2022-07-29 19:57:21 +0800

    trying to add left/right swipe
    Using code from stack overflow
    
    https://stackoverflow.com/questions/21349984/how-to-make-bootstrap-carousel-slider-use-mobile-left-right-swipe

--- scripts/script.js
+++ scripts/script.js
@@ -52,3 +52,21 @@ $(document).ready(function(){
     $('#navbar').load("navbar.html");
     $('#footer').load("footer.html");
  });
+
+ $('.carousel').on('touchstart', function(event){
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one('touchmove', function(event){
+        const xMove = event.originalEvent.touches[0].pageX;
+        const sensitivityInPx = 5;
+
+        if( Math.floor(xClick - xMove) > sensitivityInPx ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+            $(this).carousel('prev');
+        }
+    });
+    $(this).on('touchend', function(){
+        $(this).off('touchmove');
+    });
+});
